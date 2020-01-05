d3ed0bbd9892907f6809e3c240fa272b292ffdd9
evertondg <evertondg@hotmail.com.br>
2020-01-05 10:09:59 -0300

    feat: Página Carrinho concluída e estilizada

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,30 +1,67 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
-  // return (
-  //   <Container>
-  //     <ProductTable>
-  //       <thead>
-  //         <tr>
-  //           <th />
-  //           <th>Produto</th>
-  //           <th>QTD</th>
-  //           <th>SUBTOTAL</th>
-  //           <th />
-  //         </tr>
-  //       </thead>
-  //     </ProductTable>
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
 
-  //     <footer>
-  //       <button type="button">Finalizar pedido</button>
-  //       <Total>
-  //         <span>Total</span>
-  //         <strong>R$1926,23</strong>
-  //       </Total>
-  //     </footer>
-  //   </Container>
-  // );
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1926,23</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
