811e91360031e07625b34b0e0457b44ed1f3fb48
evertondg <evertondg@hotmail.com.br>
2020-01-05 09:23:08 -0300

    feat: Página Home com estilos aplicados

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Cart() {
   return <h1>Carrinho</h1>;
+  // return (
+  //   <Container>
+  //     <ProductTable>
+  //       <thead>
+  //         <tr>
+  //           <th />
+  //           <th>Produto</th>
+  //           <th>QTD</th>
+  //           <th>SUBTOTAL</th>
+  //           <th />
+  //         </tr>
+  //       </thead>
+  //     </ProductTable>
+
+  //     <footer>
+  //       <button type="button">Finalizar pedido</button>
+  //       <Total>
+  //         <span>Total</span>
+  //         <strong>R$1926,23</strong>
+  //       </Total>
+  //     </footer>
+  //   </Container>
+  // );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,131 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF">
+              3
+            </MdShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF">
+              3
+            </MdShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF">
+              3
+            </MdShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF">
+              3
+            </MdShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF">
+              3
+            </MdShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF">
+              3
+            </MdShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>
+          Tênis muito legal com um título muito grande que quebra duas linhas
+        </strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF">
+              3
+            </MdShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
